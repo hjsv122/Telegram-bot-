a907886083adc22d58f26d6f15d5e6e03c9a508f
hjsv122 <lwyalftal@gmail.com>
2025-08-17 02:11:28 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# توكن البوت من BotFather
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8235825268:AAGYbrIWSUyRpn-s2Z-MHd_4PNonQF4RvAs")
+
+# اسم البوت
+BOT_NAME = os.getenv("BOT_NAME", "@USDT_Jump_Bot")
+
+# رابط Adsterra الخاص بك
+ADSTERRA_LINK = os.getenv("ADSTERRA_LINK", "https://www.profitableratecpm.com/a87edt9y?key=bbbfda43d41c08f0e9c6c111e8903ac5")
