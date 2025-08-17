50a3d599b12724deb4834a7220f53237b3915109
hjsv122 <lwyalftal@gmail.com>
2025-08-18 00:58:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
 
 # توكن البوت من BotFather
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8235825268:AAGYbrIWSUyRpn-s2Z-MHd_4PNonQF4RvAs")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8201021725:AAHdcDm2I-SfYPgFeYo1sFY1UjpVrFKPxTo")
 
 # اسم البوت
-BOT_NAME = os.getenv("BOT_NAME", "@USDT_Jump_Bot")
+BOT_NAME = os.getenv("BOT_NAME", "@AlQimah2025Bot")
 
 # رابط Adsterra الخاص بك
 ADSTERRA_LINK = os.getenv("ADSTERRA_LINK", "https://www.profitableratecpm.com/a87edt9y?key=bbbfda43d41c08f0e9c6c111e8903ac5")
+
+# نسبة رسوم السحب (مثال: 2%)
+WITHDRAW_FEE_PERCENT = 2
